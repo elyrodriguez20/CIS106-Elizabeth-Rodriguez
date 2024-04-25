29ca5dbb20bf59380044ac1982ac601950a7e644
Ely Rodriguez <160087151+elyrodriguez20@users.noreply.github.com>
2024-04-24 19:06:53 -0500

    Python

---
+++ PS8P3.py
@@ -0,0 +1,23 @@
+#Connect to the file
+f= open("PS8P3.txt" , "r")
+name= f.readline()
+salary=0
+bonusrate=0
+bonus=0
+totalbonus=0
+count=0
+#loop as long as the last readline is not null
+while name !="":
+  salary= float(f.readline())
+  if salary>= 100000:
+    bonus= salary*0.20
+  else:
+    if salary>=50000:
+      bonus= salary*0.15
+    else:
+      bonus= salary*0.10
+
+  totalbonus= totalbonus+bonus
+  print(name, "has a salary of", salary, "and a bonus of", bonus)
+  name= f.readline().rstrip('\n')
+print("The total bonus is", totalbonus)
---
+++ PS8P4.py
@@ -0,0 +1,18 @@
+#Connect to the file
+f= open("PS8P4.txt" , "r")
+item= f.readline(). rstrip('\n')
+total=0
+count=0
+#loop as long as the last readline is not null
+while item !="":
+  quantity= int(f.readline())
+  price= float(f.readline())
+  extendedprice= quantity*price
+  total= total+extendedprice
+  print(item, "has a quantity of", quantity, "and a price of", price)
+  item= f.readline(). rstrip('\n')
+  #Increment the count
+  count= count+1
+  average_cost= total/count
+print("The total is", total)
+print("The average cost is", average_cost)
